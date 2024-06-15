cdc1426ff198a1d958d8680c4781d7839cfc32be
Kentaro Barnes <kentaro.barnes5@gmail.com>
2024-06-14 21:16:44 -0400

    express

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 const PORT: number = 3001;
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on: ${PORT}`);
