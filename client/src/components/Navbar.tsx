f83667277fcaef3c50e839269b5a30b2230b9966
Kushagra Bharti <kushagrabharti@gmail.com>
2024-06-17 01:42:15 -0500

    test1

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import logo from "../assets/companyLogo.jpg";
 
+// Navbar Component
+
 const Navbar = () => {
   return (
     <div className="navbar">
